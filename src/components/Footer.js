8649a523685e8b3f3703777264c31d11b764ee3c
rossgeorge <84723330+rossgeorge@users.noreply.github.com>
2021-05-24 16:43:37 +0100

    footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
    <footer>
 		 <p>
-			 Made by <a href="https://rossgeorge.co.uk">Ross</a> 
+			 Made by <a href="http://rossgeorge.co.uk">Ross</a> 
 		 	<a href="https://github.com/ross93ft"><img src="/github.png" alt="Github icon" /></a>
 		 </p>
 	</footer>
