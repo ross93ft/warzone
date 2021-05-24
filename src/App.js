048992910c6a1eca158f0ed8bb9d6899e750ca8f
rossgeorge <84723330+rossgeorge@users.noreply.github.com>
2021-05-24 16:20:42 +0100

    disable warning

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect} from "react";
 import './styles/app.css';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
